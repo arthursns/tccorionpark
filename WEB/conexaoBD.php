18a11d216968adf3981f489789a826f00f3f16ad
Arthur Silva <arthursns@gmail.com>
2020-10-10 20:57:24 -0300

    Atualizando arquivo de Conexão ao Banco de Dados

--- WEB/conexaoBD.php
+++ WEB/conexaoBD.php
@@ -1,11 +1,17 @@
 <?php
-$serverName = "serverName\\sqlexpress"; //serverName\instanceName
+$serverName = "serverName\\sqlexpress"; //NomeDoServidor\\NomeDoBanco  Alterar isso antes de tentar rodar!!!!!!!!!!
 
-// Since UID and PWD are not specified in the $connectionInfo array,
-// The connection will be attempted using Windows Authentication.
+//Conexão com o banco utilizando o usuário do Windows
 $connectionInfo = array( "Database"=>"dbName");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
+/*
+//Conexão com o banco utilizando um usuário especifico do SQL Server
+
+$serverName = "serverName\\sqlexpress"; //NomeDoServidor\\NomeDoBanco  Alterar isso antes de tentar rodar!!!!!!!!!!
+$connectionInfo = array( "Database"=>"dbName", "UID"=>"USUARIO", "PWD"=>"SENHA");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+*/
 
 // Tratamento de erros
 if( $conn == false ) {
