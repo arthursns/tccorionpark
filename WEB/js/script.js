51b9bf7149e1a4bb89b68d83d3d605f7f7e39a22
unknown <rrmiguel018@gmail.com>
2020-10-17 19:57:20 -0300

    atualização web 17/10/2020

--- WEB/js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navzao");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
