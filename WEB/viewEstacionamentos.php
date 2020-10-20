1fad52e2a52e382035a0a2c36f706ccd2e08c7e3
Arthur Silva <arthursns@gmail.com>
2020-10-20 08:45:18 -0300

    Correção SQL e WEB pt2

---
+++ WEB/viewEstacionamentos.php
@@ -0,0 +1,23 @@
+<?php
+include ("conexaoBD.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Estacionamentos Cadastrados</title>
+</head>
+<body>
+Atualmente, temos <?php echo $numEstacionamentos ?> estacionamentos cadastrados conosco, segue a lista deles:
+<table>
+    <tr>
+        <th>Nome fantasia</th>
+        <th>Número de vagas</th>
+        <th>Cidade</th>
+    </tr>
+    <tr>
+        
+    </tr>
+</table>
+</body>
+</html>
