8ab26a3a94f77bb484b20bc47be6b7c498141716
Alekinhoo <xandy.dre@gmail.com>
2020-11-01 22:46:41 -0300

    validações no cadastro do estacionamento

--- WEB/cadEstacionamento.php
+++ WEB/cadEstacionamento.php
@@ -11,6 +11,97 @@
     <script src="js/mask.js" type="text/javascript" ></script>
     <script src="js/script.js" type="text/javascript" ></script>
 </head>
+<script language="javascript" type="text/javascript">
+function validar() {
+var nome = form1.nome_fantasia.value;
+var nome1 = form1.razao_social.value;
+var nome2 = form1.cnpj.value;
+var nome3 = form1.telefoneEstacionamento.value; 
+var nome4 = form1.rua.value; 
+var nome5 = form1.cidade.value; 
+var nome6 = form1.bairro.value; 
+var nome7 = form1.numero.value; 
+var nome8 = form1.nome.value; 
+var nome9 = form1.email.value; 
+var nome10 = form1.usuario.value; 
+var nome11 = form1.cargo.value; 
+var nome12 = form1.senha.value; 
+var nome13 = form1.telefoneUsuario.value; 
+if (nome == "" ) {
+    alert('Preencha o campo com o Nome Fantasia do Estacionamento!');
+form1.nome_fantasia.focus();
+return false;
+}
+else if (nome1 == "" ) {
+    alert('Preencha o campo com a Razao Social do Estacionamento!');
+form1.razao_social.focus();
+return false;
+}
+
+else if (nome2 == "" ) {
+    alert('Preencha o campo com o CNPJ do Estacionamento!');
+form1.cnpj.focus();
+return false;
+}
+
+else if (nome3 == "" ) {
+    alert('Preencha o campo com o Telefone do Estacionamento!');
+form1.telefoneEstacionamento.focus();
+return false;
+}
+else if (nome4 == "" ) {
+    alert('Preencha o campo com a Rua do Estacionamento!');
+form1.rua.focus();
+return false;
+}
+else if (nome5 == "" ) {
+    alert('Preencha o campo com o Cidade do Estacionamento!');
+form1.cidade.focus();
+return false;
+}
+else if (nome6 == "" ) {
+    alert('Preencha o campo com o Bairro do Estacionamento!');
+form1.bairro.focus();
+return false;
+}
+else if (nome7 == "" ) {
+    alert('Preencha o campo com o Numero do Usuario!');
+form1.numero.focus();
+return false;
+}
+else if (nome8 == "" ) {
+    alert('Preencha o campo com o Nome do Usuario!');
+form1.nome.focus();
+return false;
+}
+else if (nome9 == "" ) {
+    alert('Preencha o campo com o Email do Usuario!');
+form1.email.focus();
+return false;
+}
+else if (nome10 == "" ) {
+    alert('Preencha o campo com o Nome/Apelido do Usuário!');
+form1.usuario.focus();
+return false;
+}
+else if (nome11 == "" ) {
+    alert('Preencha o campo com o Cargo do Usuario!');
+form1.cargo.focus();
+return false;
+}
+else if (nome12 == "" ) {
+    alert('Preencha o campo com o Senha do Usuario!');
+form1.senha.focus();
+return false;
+}
+else if (nome13 == "" ) {
+    alert('Preencha o campo com o telefone do Usuario!');
+form1.telefoneUsuario.focus();
+return false;
+}
+
+}
+</script>
 
 <!-- 
 	Acho legal você dividir o cadastro em 2 partes, aí você faz da forma que achar que fica mais legal no design.
@@ -22,11 +113,11 @@
 
     <div class="login-page">
         <div class="main-content form">
-                <form class="login-form" action="cadEstacionamentoModel.php" method="POST">
+                <form class="login-form" action="cadEstacionamentoModel.php" method="POST" name="form1">
                     <label class="orion" for="" style="color: black;">Dados do Estacionamento:</label>
                     <input type="text" name="nome_fantasia" placeholder="Nome Fantasia">
                     <input type="text" name="razao_social" placeholder="Razão Social">
-                    <input type="text" name="cnpj" placeholder="CNPJ" id="cnpj" class="cnpj">
+                    <input type="text" name="" placeholder="CNPJ" id="cnpj" class="cnpj">
                     <input type="text" name="telefoneEstacionamento" placeholder="Telefone" id="telefoneEstacionamento">
 
                     <label class="orion" for="" style="color: black;">Endereço:</label>
@@ -73,7 +164,7 @@
                 <input type="password" name="senha" placeholder="Senha">
                 <input type="text" name="telefoneUsuario" placeholder="Telefone" id="telefoneUsuario">
 
-                <button type="submit">Cadastrar</button>
+                <button type="submit" onclick="return validar()">Cadastrar</button>
 
                 <p class="message">Ja está registrado? <a href="loginEstacionamento.php">Entrar</a></p>
                 <p class="message"><a href="index.php">Voltar ao ínicio</a></p>
