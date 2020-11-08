3ed8a57a76ec9de08c09eb675e38eff3de3db392
unknown <rrmiguel018@gmail.com>
2020-11-08 15:52:19 -0300

    formatando editFuncionario

--- WEB/editFuncionario.php
+++ WEB/editFuncionario.php
@@ -40,11 +40,12 @@ $exec5 = sqlsrv_query($conn, $selectNivelAcesso);
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css/viewEstacionamento.css">
 <link rel="stylesheet" href="css/cadcupons.css">
+<link rel="stylesheet" href="css/editfucionario.css">
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="js/JQuery.js" type="text/javascript" ></script>
-<script src="js/mask.js" type="text/javascript" ></script>
-<script src="js/script.js" type="text/javascript" ></script>
+<script src="js/JQuery.js" type="text/javascript"></script>
+<script src="js/mask.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -63,50 +64,72 @@ $exec5 = sqlsrv_query($conn, $selectNivelAcesso);
     </div>
 
     <div class="content">
-        <h2>Edição Cupom</h2>
+        <h2>Edição Funcionário</h2>
     </div>
     <div class="content">
         <div class="row">
             <form action="editFuncionarioModel.php" method="POST">
-                    <h1>Dados Pessoais</h1>
-                    <label for="fname">Nome</label>
-                    <input type="text" id="fname" name="nome" placeholder="Nome" maxlength="255" value="<?php echo $dado['nome'] ?>">
-                    <label for="email">Email</label>
-                    <input type="text" id="lname" name="email" placeholder="Email" maxlength="100" value="<?php echo $dado['email']?>">
-                    <label>Telefone</label>
-                    <input type="text" id="telefoneUsuario" name="telefone" placeholder="DDD + Telefone Ex: (1293456789)" value="<?php echo $dado2['numero']?>">
-                    <label>Cargo</label>
-                    <select name="cargoId" id="cargo">
-                        <option value="">Selecione um cargo</option>
-                        <?php while($cargos = sqlsrv_fetch_array($exec4)){?>
-                        <option <?php if($dado['id_cargo'] == $cargos['id_cargo']){echo 'selected';}?> value="<?php echo $cargos['id_cargo']; ?>">
-                            <?php echo $cargos['descricao']; ?>
-                        </option>
-                    <?php }?>
-                    </select>
-                    <a href="cadCargo.php">Não encontrou o cargo? Clique aqui para cadastra-lo</a>
+                <h1>Dados Pessoais</h1>
+                <div class="text">
+                    <div class="item">
+                        <p>Nome</p>
+                        <input type="text" id="fname" name="nome" placeholder="Nome" maxlength="255" value="<?php echo $dado['nome'] ?>">
+                    </div>
+                    <div class="item">
+                        <p>Email</p>
+                        <input type="text" id="lname" name="email" placeholder="Email" maxlength="100" value="<?php echo $dado['email'] ?>">
+                    </div>
+                    <div class="item">
+                        <p>Telefone</p>
+                        <input type="text" id="telefoneUsuario" name="telefone" placeholder="DDD + Telefone Ex: (1293456789)" value="<?php echo $dado2['numero'] ?>">
+                    </div>
+                    <div class="item">
+                        <p>Cargo</p>
+                        <select name="cargoId" id="cargo">
+                            <option value="">Selecione um cargo</option>
+                            <?php while ($cargos = sqlsrv_fetch_array($exec4)) { ?>
+                                <option <?php if ($dado['id_cargo'] == $cargos['id_cargo']) {
+                                            echo 'selected';
+                                        } ?> value="<?php echo $cargos['id_cargo']; ?>">
+                                    <?php echo $cargos['descricao']; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="item">
+
+                        <a href="cadCargo.php">Não encontrou o cargo? <b>Clique aqui</b> para cadastra-lo</a>
+                    </div>
+                    
                     <h1>Conta para acesso ao Gerenciador</h1>
-                    <label>Usuario</label>
-                    <input type="text" id="fname" name="usuario" placeholder="Usuário" maxlength="255" value="<?php echo $dado3['usuario']; ?>">
-                    <label>Senha</label>
-                    <br>
-                    <input type="password" id="fname" name="senha" placeholder="Deixar vazio se não for alterar">
-                    <br>
-                    <br>
-                    <label>Nível de Acesso</label>
-                    <select name="nivelAcesso" id="nivelAcesso">
-                        <option value="">Selecione um Nível de Acesso</option>
-                                            <?php while ($nivelAcesso = sqlsrv_fetch_array($exec5)) { ?>
-                        <option <?php if($dado3['id_nivel_acesso'] == $nivelAcesso['id_nivel_acesso']) {echo 'selected'; } ?> value="<?php echo $nivelAcesso['id_nivel_acesso']; ?>">
-                            <?php echo $nivelAcesso['descricao']; ?>
-                        </option>
-                        <?php } ?>
-                    </select>
-                    <input type="hidden" name="id_login" value="<?php echo $dado['id_login']?>">
-                    <input type="hidden" name="id_telefone" value="<?php echo $dado['id_telefone']?>">
-                    <input type="hidden" name="id_responsavel" value="<?php echo $idFuncionario ?>">
-                    <input type="submit" value="Editar">
-                </form>
+                    <div class="item">
+                        <p>Usuário</p>
+                        <input type="text" id="fname" name="usuario" placeholder="Usuário" maxlength="255" value="<?php echo $dado3['usuario']; ?>">
+                    </div>
+                    <div class="item">
+                        <p>Senha</p>
+                        <input type="password" id="fname" name="senha" placeholder="Deixar vazio se não for alterar">
+                    </div>
+                    <div class="item">
+                        <p>Nível Acesso</p>
+                        <select name="nivelAcesso" id="nivelAcesso">
+                            <option value="">Selecione um Nível de Acesso</option>
+                            <?php while ($nivelAcesso = sqlsrv_fetch_array($exec5)) { ?>
+                                <option <?php if ($dado3['id_nivel_acesso'] == $nivelAcesso['id_nivel_acesso']) {
+                                            echo 'selected';
+                                        } ?> value="<?php echo $nivelAcesso['id_nivel_acesso']; ?>">
+                                    <?php echo $nivelAcesso['descricao']; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+                
+                <input type="hidden" name="id_login" value="<?php echo $dado['id_login'] ?>">
+                <input type="hidden" name="id_telefone" value="<?php echo $dado['id_telefone'] ?>">
+                <input type="hidden" name="id_responsavel" value="<?php echo $idFuncionario ?>">
+                <input type="submit" value="Editar">
+            </form>
         </div>
     </div>
 
